bde748ed4dfa5752df8ed1fcc531e786bc59526a
Aryan Chaudhary <aryanchaudhary985@gmail.com>
2025-02-09 11:22:04 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Link from 'next/link';
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>
-        Click Me
-      </Button>
+      Click <Link href=" /document"><span className='text-blue-500 underline'>&nbsp;here&nbsp;</span></Link> to go to document id
     </div>
   );
 }
